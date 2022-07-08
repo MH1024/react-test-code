45bda13d885b1de339e4a90b358246fa86d58fdb
Sam <huangsaiy@hotmail.com>
2022-07-08 10:01:37 +1000

    [WIP]add route and homepage draft

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Row } from 'reactstrap'
+const Menu = () => {
+    return (
+        <div className="prod-list section">
+            <Row>
+                <Button>Place Order</Button>
+            </Row>
+        </div>
+    )
+}
+export default Menu
